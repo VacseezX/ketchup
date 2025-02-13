7c478366417c14c0deb7f882a263de47e1747e0a
VacseezX <c2vacger@vasvari.org>
2025-02-13 11:17:05 +0100

    Beolvasás def

--- vasarlas.py
+++ vasarlas.py
@@ -1,1 +1,9 @@
-f = open("vasarlas.csv", "r")
+haviKoltes = []
+
+def beolvasas(lista:list):
+    f = open("vasarlas.csv", "r")
+    lista.append(f.readline().strip("\n").split(";"))
+    return lista
+
+haviKoltes = beolvasas(haviKoltes)
+print(haviKoltes)
