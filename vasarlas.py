dd4efee072d9e5edd4bad625840e926b6117f9d7
VacseezX <c2vacger@vasvari.org>
2025-02-13 11:26:58 +0100

    Napok számlálása

--- vasarlas.py
+++ vasarlas.py
@@ -1,9 +1,18 @@
-haviKoltes = []
+honap = []
+napokSzama = 0
 
 def beolvasas(lista:list):
     f = open("vasarlas.csv", "r")
-    lista.append(f.readline().strip("\n").split(";"))
+    lista = (f.readline().strip("\n").split(";"))
     return lista
 
-haviKoltes = beolvasas(haviKoltes)
-print(haviKoltes)
+def napok(lista:list):
+    napokSzama = 0
+    for napok in lista:
+        napokSzama += 1
+    return napokSzama
+
+honap = beolvasas(honap)
+print(honap)
+napokSzama = napok(honap)
+print(napokSzama)
